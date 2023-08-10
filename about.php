5d95e81ab112af8e35d29d6109f8dcf8e592e84e
austinplatt <austinplatt@u.boisestate.edu>
2023-08-09 21:15:07 -0700

    Update

--- about.php
+++ about.php
@@ -1,17 +1,23 @@
 <?php include("header.php"); ?>
 <?php include("nav.php"); ?>
-<div id="content">
-	<main>
-                <section>
-                        <h2>OUR MISSION</h2>
-                        <p>We are a fitness center dedicated to helping people reach their health and fitness goals. Our trainers are experienced and knowledgeable and will work with you to develop a personalized fitness plan that works for you.</p>
-                </section>
-                <section>
-                        <h2>DISCOVER THE TRUTH IN FITNESS</h2>
-                        <p> At PlatinumPlatt Fitness, we pride ourselves in being a locally owned and operated fitness club. Through your membership at PlatinumPlatt, you will recieve the necessary tools and skills to achieve your ideal physique. Our passionate and knowledgeable coaching staff will strive to help you reach your goals, both inside and outside the gym.</p>
-                </section>
-		<section>
-			<h3>CLUB HOURS</h3>
+</div>
+<div class="page-content">
+     <h2 class="section-title">About Us</h2>
+        <div class="section-divider"></div>
+
+        <!-- First two divs, one below the other -->
+        <div class="textbox">    
+        <h3>OUR MISSION</h3>
+        <p>We are a fitness center dedicated to helping people reach their health and fitness goals. Our trainers are experienced and knowledgeable and will work with you to develop a personalized fitness plan that works for you.</p>
+        </div>
+        <div class="textbox">
+             <h3>DISCOVER THE TRUTH IN FITNESS</h3>
+             <p> At PlatinumPlatt Fitness, we pride ourselves in being a locally owned and operated fitness club. Through your membership at PlatinumPlatt, you will recieve the necessary tools and skills to achieve your ideal physique. Our passionate and knowledgeable coaching staff will strive to help you reach your goals, both inside and outside the gym.</p>
+        </div>
+        <!-- Next two divs, side by side -->
+        <div class="side-by-side">
+            <div class="textbox">
+	         <h3>CLUB HOURS</h3>
                         <ul>
                                 <li>MONDAY            5:00 AM - 12:00 AM</li>
                                 <li>TUESDAY            5:00 AM - 12:00 AM</li>
@@ -21,15 +27,16 @@
 				<li>SATURDAY            5:00 AM - 10:00 PM</li>
 				<li>SUNDAY            5:00 AM - 9:00 PM</li>
                         </ul>			
-                <section>
-                        <h3>BUISNESS INFORMATION & LOCATIONS</h3>
+             </div>
+             <div class="textbox">
+                  <h3>BUISNESS INFORMATION & LOCATIONS</h3>
                         <h4>BOISE</h4>
 			<p>1234 W Boise Ave, Boise ID 83706</p>
 			<h4>PHONE</h4>
 			<p>(208)111-2222</p>
 			<h4>EMAIL</h4>
 			<p>platinumplattfitness@boise.com</p>
-                </section>
+                </div>
         </main>
 </div>
 <?php include("footer.php"); ?>
