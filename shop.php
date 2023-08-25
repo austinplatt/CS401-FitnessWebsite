abd145aa81423a413b5abb9c696ebf65d331ff87
austinplatt <austinplatt@u.boisestate.edu>
2023-08-24 21:35:16 -0600

    "Updated Files"

---
+++ shop.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>PlatinumPlatt Fitness</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+        <div class="header">
+		<?php include 'header.php'; ?>
+	</div>
+
+
+        <div class="navigation">
+	        <?php include('nav.php'); ?>
+	</div>
+
+        <body>
+
+        <div class="mainBody">
+        
+                <div class="bodyContainer">
+
+                        <div class="left-image">
+                                <img src="images\fitness.jpeg" alt="Fitness Image">
+
+                        </div>
+                        
+                        <div class="main-text">
+                        
+                                <div class="shirt-image1">               
+                                      
+                                </div>
+                                
+                                <div class="shirt-image2">
+                                        
+                                </div>
+
+                                <div class="club-info-Container">
+
+                                        <div class="club-info">
+                                                <h3>CLUB HOURS</h3>
+                                                <ul>
+                                                        <li>MONDAY            5:00 AM - 12:00 AM</li>
+                                                        <li>TUESDAY            5:00 AM - 12:00 AM</li>
+                                                        <li>WEDNESDAY            5:00 AM - 12:00 AM</li>
+                                                        <li>THURSDAY            5:00 AM - 12:00 AM</li>
+                                                        <li>FRIDAY            5:00 AM - 12:00 AM</li>
+                                                        <li>SATURDAY            5:00 AM - 10:00 PM</li>
+                                                        <li>SUNDAY            5:00 AM - 9:00 PM</li>
+                                                </ul>
+                                        </div>
+
+                                        <!-- TODO  -->
+                                        <div class="club-info">
+                                                <h3>BUSINESS INFORMATION & LOCATIONS</h3>
+                                                <h4>BOISE</h4>
+                                                        1234 W Boise Ave, Boise ID 83706
+                                                <h4>PHONE</h4>
+                                                        (208)111-2222
+                                                <h4>EMAIL</h4>
+                                                        platinumplattfitness@boise.com
+                                        </div>
+                                </div>
+                        
+                        </div>
+                        
+                </div>
+        </div>
+
+        
+        </body>
+
+        <div class="footer">
+                <?php include("footer.php"); ?>
+        </div>
+</html>
