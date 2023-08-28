2d398230d891fdd4593df5403aaa9cd52328423a
austinplatt <austinplatt@u.boisestate.edu>
2023-08-27 20:51:00 -0600

    "Changes to Nav bar"

--- nav.php
+++ nav.php
@@ -1,8 +1,17 @@
+<?php session_start(); ?>
 <div class="topnav">
-        <a href="index.php">HOME</a>
-        <a href="about.php">ABOUT US</a>
-        <a href="training.php">TRAINING</a>
-        <a href="workouts.php">WORKOUTS</a>
-        <a href="shop.php">SHOP</a>
-	<a href="signin.php">SIGN-IN</a>
+    <a href="index.php">HOME</a>
+    <a href="about.php">ABOUT US</a>
+    <a href="training.php">TRAINING</a>
+    <a href="workouts.php">WORKOUTS</a>
+    <a href="shop.php">SHOP</a>
+
+    <?php
+        // Check if the user is authenticated
+        if (isset($_SESSION['authenticated']) && $_SESSION['authenticated'] === true) {
+            echo '<a href="logout.php">LOGOUT</a>';
+        } else {
+            echo '<a href="signin.php">SIGN-IN</a>';
+        }
+    ?>
 </div>
