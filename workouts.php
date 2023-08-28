c4c44ec8acc5e48b4ebd066d5a6d95ba5c209c61
austinplatt <austinplatt@u.boisestate.edu>
2023-08-27 20:54:54 -0600

    "Updated workouts page"

--- workouts.php
+++ workouts.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-        <title>Workouts</title>
+        <title>PlatinumPlatt Fitness | Workouts</title>
         <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="icon" type="image/png" href="images/favicon.ico">
 </head>
 
         <div class="header">
@@ -20,25 +21,46 @@
         
                 <div class="bodyContainer">
 
-                        <div class="left-image"> 
-                                        <img src="images/fitness.jpg" alt="Les Mills Image"> 
-                        </div>
+                        <div class="leftContainer">
 
-                        <div class="right-image">
-                                        <img src="images/fitness.jpg" alt="Matrix Image">
-                        </div>
-                        
-                        <div class="main-text">
+
+                
+
+                                <div class="left-image"> 
+                                        <img src="images/lesMills.jpeg" alt="Les Mills Image"> 
+                                </div>
+
+                                <div class="main-text">
                         
                                 <div class="section-title">               
                                         <h2>LES MILLS</h2>
                                         <p>Access limitless LES MILLS on Demand content - enjoy your preferred workouts whenever and wherever you like. Save 33% on your monthly subscription fees and relish your initial 60 days absolutely FREE.</p>
                                 </div>
-                                
-                                <div class="section-title">
-                                        <h2>MATRIX FITNESS</h2>
-                                        <p> Matrix offers an extensive collection of exercises that provide detailed demonstrations to cater to your overall body fitness needs. Regardless of your distinct objectives or skill level, our exercise library is designed to enhance strength, cardiovascular health, flexibility, and other aspects, all while honing proper form and technique.</p>
                                 </div>
+
+
+                        </div>
+
+
+                        <div class="rightContainer">
+                                <div class="right-image">
+                                        <img src="images/matrixWorkout.jpeg" alt="Matrix Image">
+                                </div>
+
+                                <div class="main-text">
+                                        <div class="section-title">
+                                                <h2>MATRIX FITNESS</h2>
+                                                <p> Matrix offers an extensive collection of exercises that provide detailed demonstrations to cater to your overall body fitness needs. Regardless of your distinct objectives or skill level, our exercise library is designed to enhance strength, cardiovascular health, flexibility, and other aspects, all while honing proper form and technique.</p>
+                                        </div>
+                                </div>
+                        </div>
+                        
+
+                        
+
+
+                        <div class="club-info-Container">
+
                                 <div class="club-info">
                                         <h3>CLUB HOURS</h3>
                                         <ul>
@@ -49,18 +71,21 @@
                                                 <li>FRIDAY            5:00 AM - 12:00 AM</li>
                                                 <li>SATURDAY            5:00 AM - 10:00 PM</li>
                                                 <li>SUNDAY            5:00 AM - 9:00 PM</li>
-                                        </ul>			
+                                        </ul>
+                                </div>
+
+                                
                                 <div class="club-info">
                                         <h3>BUSINESS INFORMATION & LOCATIONS</h3>
                                         <h4>BOISE</h4>
-                                        <p>1234 W Boise Ave, Boise ID 83706</p>
+                                                1234 W Boise Ave, Boise ID 83706
                                         <h4>PHONE</h4>
-                                        <p>(208)111-2222</p>
+                                                (208)111-2222
                                         <h4>EMAIL</h4>
-                                        <p>platinumplattfitness@boise.com</p>
+                                                platinumplattfitness@boise.com
                                 </div>
-                        
                         </div>
+
                         
                 </div>
         </div>
