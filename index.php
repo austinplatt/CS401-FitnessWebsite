c9a66bcafbac2ac74172d792dcbb93b902676f8c
austinplatt <austinplatt@u.boisestate.edu>
2023-08-27 20:49:40 -0600

    "Updated index Changes"

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<title>PlatinumPlatt Fitness</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="icon" type="image/png" href="images/favicon.ico">
 </head>
 
 <body>
@@ -25,8 +26,33 @@
 		
 		
 			<h2 class="section-title">PlatinumPlatt Fitness</h2>
+
+
+			<?php
+			if (isset($_SESSION['authenticated']) && $_SESSION['authenticated']) {
+				echo "Welcome, " . $_SESSION['email'] . "!";
+			}
+			?>
+
 			<p class="section-description">This website is designed to help you achieve your fitness goals. We offer a variety of workout programs, meal plans, and fitness tips to help you get in the best shape of your life.</p>
-			<button class="button">GET STARTED</button>
+
+			
+
+			<button id="getStartedBtn" class="button">GET STARTED</button>
+
+			<script>
+			document.getElementById("getStartedBtn").addEventListener("click", function() {
+				<?php
+				
+				if (isset($_SESSION['authenticated'])) {
+					echo 'window.location.href = "workouts.php";';
+				} else {
+					echo 'window.location.href = "signin.php";';
+				}
+				?>
+			});
+			</script>
+
 		
 	</div>
 	
