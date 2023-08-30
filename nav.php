773092407edb6e7725306544e18cf0ec2816f608
austinplatt <austinplatt@u.boisestate.edu>
2023-08-30 14:49:09 -0600

    "Updates to Navigation"

--- nav.php
+++ nav.php
@@ -2,20 +2,23 @@
 if (empty(session_id()) && !headers_sent()) {
     session_start();
 }
+
+$current_page = basename($_SERVER['PHP_SELF']);
+
 ?>
 <div class="topnav">
-    <a href="index.php">HOME</a>
-    <a href="about.php">ABOUT US</a>
-    <a href="training.php">TRAINING</a>
-    <a href="workouts.php">WORKOUTS</a>
-    <a href="shop.php">SHOP</a>
+    <a href="index.php" class="<?php echo $current_page == 'index.php' ? 'active' : ''; ?>">HOME</a>
+    <a href="about.php" class="<?php echo $current_page == 'about.php' ? 'active' : ''; ?>">ABOUT US</a>
+    <a href="training.php" class="<?php echo $current_page == 'training.php' ? 'active' : ''; ?>">TRAINING</a>
+    <a href="workouts.php" class="<?php echo $current_page == 'workouts.php' ? 'active' : ''; ?>">WORKOUTS</a>
+    <a href="shop.php" class="<?php echo $current_page == 'shop.php' ? 'active' : ''; ?>">SHOP</a>
 
     <?php
         // Check if the user is authenticated
         if (isset($_SESSION['authenticated']) && $_SESSION['authenticated'] === true) {
             echo '<a href="logout.php">LOGOUT</a>';
         } else {
-            echo '<a href="signin.php">SIGN-IN</a>';
+            echo '<a href="signin.php" class="' . ($current_page == 'signin.php' ? 'active' : '') . '">SIGN-IN</a>';
         }
     ?>
 </div>
